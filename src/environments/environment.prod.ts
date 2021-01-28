554941a7c5aae1fb059880027ad1f84f3b90b6d3
MARIO PALOMO <mariofpalomoa@gmail.com>
2021-01-28 18:03:48 -0500

    v2 28012021
    v2 28012021

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   conexionVista: 'https://www.remolkar.com/',
-  //conexionApi: 'https://servicio.remolkar.com/',
-  conexionApi: 'http://localhost:3105/',
+  conexionApi: 'https://servicio.remolkar.com/',
+  //conexionApi: 'http://localhost:3105/',
   conexionImagenes: 'https://www.remolkar.com/imagenes/productos/',
   conexionVideos: 'https://www.remolkar.com/imagenes/videos/',
   conexionDatafast: "https://test.oppwa.com/v1/paymentWidgets.js?checkoutId=",
