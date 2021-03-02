abc1cd9ab1dc53e5755205c98e88d13495e53625
SSTECEC <admin@sstecec.com>
2021-03-02 14:25:49 -0500

    v1 02/03/2021
    v1 02/03/2021

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,6 @@ export const environment = {
   production: true,
   conexionVista: 'https://www.remolkar.com/',
   conexionApi: 'https://servicio.remolkar.com/',
-  //conexionApi: 'http://localhost:3105/',
   conexionImagenes: 'https://www.remolkar.com/imagenes/productos/',
   conexionVideos: 'https://www.remolkar.com/imagenes/videos/',
   conexionDatafast: "https://test.oppwa.com/v1/paymentWidgets.js?checkoutId=",
